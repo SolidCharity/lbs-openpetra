2da3ab2e35f93f04382827443e067d2d60a39dd9
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2014-12-23 18:10:09 +0000

    build openpetra nightlydevzip on LBS

---
+++ nightlydevzip/setup.sh
@@ -0,0 +1,19 @@
+yum -y install mono-xsp-opt mono-opt-devel wget tar
+. /opt/mono/env.sh
+wget https://github.com/openpetra/openpetra/archive/master.tar.gz
+tar xzf master.tar.gz
+mv openpetra-master nightlydevzip
+
+cd nightlydevzip
+cat > OpenPetra.build.config << EOF
+<?xml version="1.0"?>
+<project name="OpenPetra-userconfig">
+    <property name="DBMS.Type" value="sqlite"/>
+    <property name="DBMS.Password" value=""/>
+</project>
+EOF
+patch -p1 < ../startServer.patch
+nant devzip
+
+mv ../openpetra_development_`date +"%Y-%m-%d"`.zip ~/tarball
+echo download at http://download.lbs.solidcharity.com/tarballs/tpokorra/openpetra/openpetra_development_`date +"%Y-%m-%d"`.zip
