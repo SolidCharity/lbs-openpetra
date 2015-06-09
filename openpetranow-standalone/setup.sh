24915c17afc62bb64c43c8ef3cb3ff4a55b81d73
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2015-06-09 20:10:59 +0000

    standalone: add a workaround for the caching problems. related to https://github.com/openpetra/openpetra/issues/63

--- openpetranow-standalone/setup.sh
+++ openpetranow-standalone/setup.sh
@@ -3,10 +3,11 @@
 #install the key from Xamarin
 rpm --import "http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
 
-yum install -y mono-complete nant dos2unix nsis gettext
+yum install -y mono-complete nant dos2unix nsis gettext patch
 
 tar xzf ~/sources/master.tar.gz
 cd openpetra-master
+patch -p1 < ../StandaloneNoClientCache.patch
 export NSISDIR=/usr/local/nsis/
 export PATH=$NSISDIR:$PATH
 nant buildWindowsStandalone -D:OpenBuildService=true || exit -1
