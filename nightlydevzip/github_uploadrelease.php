738d43e0a3b6fd936568a19ee7cb777dfd9c8057
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2015-08-01 09:15:27 +0000

    nightly devzip: upload zip file to Github release

---
+++ nightlydevzip/github_uploadrelease.php
@@ -0,0 +1,128 @@
+<?php
+@include '/root/.ssh/github_config.php';
+
+$url = "https://api.github.com";
+date_default_timezone_set('Europe/Brussels');
+
+# get all repos of user
+if (false) {
+
+$opts = array('http' =>
+    array(
+        'method'  => 'GET',
+        'header'  => 
+                     'User-Agent: MyClient'."\r\n".
+                     'Authorization: Basic '.base64_encode($user.":".$token)."\r\n".
+                     'Accept: application/vnd.github.v3+json',
+    )
+);
+// print_r($opts);
+$context = stream_context_create($opts);
+$requesturl = $url."/users/tpokorra/repos";
+if (($result = file_get_contents($requesturl, false, $context)) === false) {
+	echo "Problem"."\n";
+	var_dump($http_response_header);
+	echo "\n";
+	die();
+}
+echo print_r(json_decode($result),true);
+}
+
+if (true) {
+# show all releases
+$opts = array('http' =>
+    array(
+        'method'  => 'GET',
+        'header'  =>
+                      'User-Agent: MyClient'."\r\n".
+                      'Authorization: Basic '.base64_encode($user.":".$token)."\r\n".
+                     'Accept: application/vnd.github.v3+json',
+    )
+);
+$context = stream_context_create($opts);
+$requesturl = $url."/repos/openpetra/openpetra-nightlydevzip/releases";
+if (($result = file_get_contents($requesturl, false, $context)) === false) {
+  echo "Problem"."\n";
+  var_dump($http_response_header);
+        echo "\n";
+        die();
+}
+$result = json_decode($result);
+echo print_r($result,true);
+}
+
+if (true) {
+# delete a release
+$releaseId = "1616067";
+$releaseId = $result[0]->id;
+$conn=curl_init();
+curl_setopt($conn, CURLOPT_CUSTOMREQUEST, 'DELETE');
+curl_setopt($conn, CURLOPT_USERAGENT, "myClient");
+curl_setopt($conn, CURLOPT_URL, $url."/repos/openpetra/openpetra-nightlydevzip/releases/$releaseId");
+curl_setopt($conn, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($conn, CURLOPT_HTTPHEADER, array(
+    'Authorization: Basic '.base64_encode($user.":".$token))
+);
+$result = curl_exec($conn);
+curl_close($conn);
+echo $result."\n";
+}
+
+
+if (true) {
+# create a new release
+# see https://developer.github.com/v3/repos/releases/
+$query_string = "";
+
+$zipname = "openpetra_development_".date("Y-m-d");
+$tagname = date("Y-m-d");
+$values = array( "tag_name" => $tagname,
+        "prerelease" => "true",
+        "target_commitish" => "master"
+    );
+$data_string = json_encode($values);
+$conn=curl_init();
+curl_setopt($conn, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($conn, CURLOPT_USERAGENT, "myClient");
+curl_setopt($conn, CURLOPT_URL, $url."/repos/openpetra/openpetra-nightlydevzip/releases");
+curl_setopt($conn, CURLOPT_POSTFIELDS, $data_string);
+curl_setopt($conn, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($conn, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'Authorization: Basic '.base64_encode($user.":".$token),
+    'Content-Length: ' . strlen($data_string))
+);
+$result = curl_exec($conn);
+curl_close($conn);
+$result = json_decode($result);
+echo print_r($result, true)."\n";
+$id = $result->id;
+$upload_url = $result->upload_url;
+# now upload the zip file
+$conn=curl_init();
+$data_string = file_get_contents('/root/lbs-openpetra/nightlydevzip/nightlydevzip/openpetra-nightlydevzip/'.$zipname.'.zip');
+$upload_url = str_replace('{?name}', '?name='.$zipname.'.zip', $upload_url);
+echo "uploading to ".$upload_url."...\n";
+curl_setopt($conn, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($conn, CURLOPT_USERAGENT, "myClient");
+curl_setopt($conn, CURLOPT_URL, str_replace('{?name}', '?name='.$zipname.'.zip', $upload_url));
+curl_setopt($conn, CURLOPT_TIMEOUT, 10*60); // seconds
+curl_setopt($conn, CURLOPT_BINARYTRANSFER, TRUE);
+curl_setopt($conn, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($conn, CURLOPT_HTTPHEADER, array(
+    'Content-Type:application/octet-stream',
+    'Content-Length: ' . strlen($data_string),
+    'Authorization: Basic '.base64_encode($user.":".$token))
+);
+#$result = curl_exec($conn);
+#curl_close($conn);
+#echo $result."\n";
+#echo print_r(json_decode($result),true)."\n";
+$cmd = 'curl -# -XPOST -H "Authorization: Basic '.base64_encode($token).'" -H "Content-Type:application/octet-stream" --data-binary @/root/lbs-openpetra/nightlydevzip/nightlydevzip/openpetra-nightlydevzip/'.$zipname.'.zip '.$upload_url;
+exec($cmd,$result);
+echo print_r($result,true)."\n";
+}
+
+
+?>
+
--- nightlydevzip/setup.sh
+++ nightlydevzip/setup.sh
@@ -4,7 +4,7 @@
 rpm --import "http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
 # get the key for the OpenPetra packages
 rpm --import "http://keyserver.ubuntu.com/pks/lookup?op=get&fingerprint=on&search=0x4796B710919684AC"
-yum -y install mono xsp mono-mvc mono-data-sqlite nant wget tar sqlite git
+yum -y install mono xsp mono-mvc mono-data-sqlite nant wget tar sqlite php-cli curl
 
 wget https://github.com/openpetra/openpetra/archive/master.tar.gz || exit -1
 tar xzf master.tar.gz
@@ -29,25 +29,9 @@ cp /usr/lib/mono/4.5/Mono.Data.Sqlite.dll csharp/ThirdParty/SQLite/
 nant devzip || exit -1
 
 #upload to Github
-if [ -f ~/.ssh/id_rsa_cronjob ]
+if [ -f ~/.ssh/github_config.php ]
 then
-  eval `ssh-agent`
-  ssh-add ~/.ssh/gitkey
-  ssh-keyscan -H github.com >> ~/.ssh/known_hosts
-  # alternatively: use releases https://developer.github.com/v3/repos/releases/#upload-a-release-asset
-  git clone --depth 1 git@github.com:openpetra/openpetra-nightlydevzip.git || exit -1
-  cd openpetra-nightlydevzip
-  git rm openpetra_development_*.zip
-  TODAY=`date +"%Y-%m-%d"`
-  cp ../../openpetra_development_$TODAY.zip .
-  git add openpetra_development_*.zip
-  git config --global user.name "LBS BuildBot"
-  git config --global user.email timotheus.pokorra@solidcharity.com
-  git commit -a -m "nightly release for $TODAY" || exit -1
-  git config --global push.default simple
-  git push || exit -1
-  cd ..
-  kill $SSH_AGENT_PID
+  php ../github_uploadrelease.php
 fi
 
 mv ../openpetra_development_`date +"%Y-%m-%d"`.zip ~/tarball
