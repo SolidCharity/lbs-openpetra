08d16e058dad28b47700ff6b329f18bf06e9b97b
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2015-09-09 22:11:29 +0200

    adjust openpetranow-standalone and openpetranow-standalone-test

--- openpetranow-standalone/setup.sh
+++ openpetranow-standalone/setup.sh
@@ -1,17 +1,43 @@
 #!/bin/bash
 
+branch=master
+branch=20150909_release_2015-09
+version=2015.09.0
+if [ ! -z "$1" ]; then
+  branch=$1
+  version=`echo $branch | awk -F_ '{print $NF}' | sed -e 's#-#.#g'`
+  version="$version.0"
+  echo "calculated version: $version"
+  if [ -z "$version" ]
+  then
+    echo "cannot make a version number out of $branch"
+    exit -1
+  fi
+fi
+
 #install the key from Xamarin
 rpm --import "http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
 
-yum install -y mono-complete nant dos2unix nsis gettext patch
+yum install -y mono-complete nant dos2unix nsis gettext patch wget
 
-tar xzf ~/sources/sources.tar.gz
+if [[ "$branch" == "master" ]]
+then
+  wget https://github.com/openpetra/openpetra/archive/$branch.tar.gz -O sources.tar.gz || exit -1
+else
+  wget https://github.com/tpokorra/openpetra/archive/$branch.tar.gz -O sources.tar.gz || exit -1
+fi
+
+tar xzf sources.tar.gz
 dir=$(find . -type d -name openpetra-*)
 cd $dir
 patch -p1 < ../StandaloneNoClientCache.patch || exit -1
 export NSISDIR=/usr/local/nsis/
 export PATH=$NSISDIR:$PATH
-nant buildWindowsStandalone -D:OpenBuildService=true || exit -1
+if [[ "$branch" == "master" ]]
+then
+  version=`cat db/version.txt`
+fi
+nant buildWindowsStandalone -D:OpenBuildService=true -D:ReleaseID=$version || exit -1
 
 path=windows/openpetranow-standalone
 mkdir -p ~/repo/$path
