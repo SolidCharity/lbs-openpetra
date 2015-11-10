7efc4b348716f72fe4a529a96571e0e9cfb9ddcd
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2015-11-10 09:51:51 +0100

    dbdoc and codedoc: need openssh-clients for uploading the result

--- codedoc/setup.sh
+++ codedoc/setup.sh
@@ -4,7 +4,7 @@
 rpm --import "http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
 # get the key for the OpenPetra packages
 rpm --import "http://keyserver.ubuntu.com/pks/lookup?op=get&fingerprint=on&search=0x4796B710919684AC"
-dnf -y install mono-core mono-devel libgdiplus-devel nant wget tar sqlite doxygen || exit -1
+dnf -y install mono-core mono-devel libgdiplus-devel nant wget tar sqlite doxygen openssh-clients || exit -1
 
 wget https://github.com/openpetra/openpetra/archive/master.tar.gz
 #wget https://github.com/tpokorra/openpetra/archive/master.tar.gz
