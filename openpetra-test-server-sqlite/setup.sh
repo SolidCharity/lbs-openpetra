83379b7e783a974333fccadd286a6b26ea37cba8
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2015-11-23 09:47:35 +0100

    nant test requires mono-winfx for WindowsBase.dll for the test Ict.Common.IO.Testing.TTestCommonIO.TestExcelExportFile

--- openpetra-test-server-sqlite/setup.sh
+++ openpetra-test-server-sqlite/setup.sh
@@ -8,7 +8,7 @@ fi
 yum install -y epel
 #install the key from Xamarin
 #rpm --import "http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
-yum install -y wget sudo mono-devel mono-mvc mono-wcf libgdiplus-devel liberation-mono-fonts nant NUnit xsp sqlite lsb
+yum install -y wget sudo mono-devel mono-mvc mono-winfx mono-wcf libgdiplus-devel liberation-mono-fonts nant NUnit xsp sqlite lsb
 
 if [[ "$branch" == "master" ]]
 then
