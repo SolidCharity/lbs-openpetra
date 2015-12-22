33664a63169dfd6afd3fe7fe9887210f744f844e
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2015-12-22 07:41:34 +0100

    openpetranow-standalone-test: fix for including i18n and plugins. fixing the release number for branch

--- openpetranow-standalone-test/setup.sh
+++ openpetranow-standalone-test/setup.sh
@@ -21,44 +21,48 @@ yum install -y mono-devel libgdiplus-devel liberation-mono-fonts nant dos2unix n
 
 wget $giturl/$branch.tar.gz -O sources.tar.gz || exit -1
 
-tar xzf sources.tar.gz
-srcdir=`pwd`
+tar xzf sources.tar.gz || exit -1
+srcdir="/root/sources"
 dir=$(find . -type d -name openpetra-*)
 cd $dir
-tar xzf $srcdir/plugin_bankimport.tar.gz && mv OpenPetraPlugin_Bankimport-master csharp/ICT/Petra/Plugins/Bankimport
-tar xzf $srcdir/plugin_bankimport_csv.tar.gz && mv OpenPetraPlugin_BankimportCSV-master csharp/ICT/Petra/Plugins/BankimportCSV
-tar xzf $srcdir/plugin_bankimport_mt940.tar.gz && mv OpenPetraPlugin_BankimportMT940-master csharp/ICT/Petra/Plugins/BankimportMT940
-tar xzf $srcdir/plugin_bankimport_camt.tar.gz && mv OpenPetraPlugin_BankimportCAMT-master csharp/ICT/Petra/Plugins/BankimportCAMT
+(tar xzf $srcdir/plugin_bankimport.tar.gz && mv OpenPetraPlugin_Bankimport-master csharp/ICT/Petra/Plugins/Bankimport) || exit -1
+(tar xzf $srcdir/plugin_bankimport_csv.tar.gz && mv OpenPetraPlugin_BankimportCSV-master csharp/ICT/Petra/Plugins/BankimportCSV) || exit -1
+(tar xzf $srcdir/plugin_bankimport_mt940.tar.gz && mv OpenPetraPlugin_BankimportMT940-master csharp/ICT/Petra/Plugins/BankimportMT940) || exit -1
+(tar xzf $srcdir/plugin_bankimport_camt.tar.gz && mv OpenPetraPlugin_BankimportCAMT-master csharp/ICT/Petra/Plugins/BankimportCAMT) || exit -1
 
-tar xzf $srcdir/i18n.tar.gz
-mv openpetra-i18n-master/i18n/de.po i18n/de_DE.po
-mv openpetra-i18n-master/i18n/es.po i18n/es_ES.po
-mv openpetra-i18n-master/i18n/da.po i18n/da_DK.po
-nant translation
+tar xzf $srcdir/i18n.tar.gz || exit -1
+mv openpetra-i18n-master/i18n/de.po i18n/de_DE.po || exit -1
+mv openpetra-i18n-master/i18n/es.po i18n/es_ES.po || exit -1
+mv openpetra-i18n-master/i18n/da.po i18n/da_DK.po || exit -1
+nant translation || exit -1
 
 export NSISDIR=/usr/local/nsis/
 export PATH=$NSISDIR:$PATH
 if [[ "$branch" == "master" ]]
 then
   version=`cat db/version.txt | awk -F. '{print $1"."$2".99"}'`
-  newrelease=0
-  if [ -d ~/repo/$path/$branch ]
-  then
-    for f in ~/repo/$path/$branch/OpenPetraSetup-$version.*.exe
-    do
-      if [ ! -f "$f" ]
-      then
-        break;
-      fi
-      release=`basename $f | awk -F. '{print $4}'`
-      if [ $release -ge $newrelease ]
-      then
-        newrelease=$((release+1))
-      fi
-    done
-  fi
-  version=$version"."$newrelease
+else
+  version=`echo $version | awk -F. '{print $1"."$2"."$3}'`
 fi
+
+newrelease=0
+if [ -d ~/repo/$path/$branch ]
+then
+  for f in ~/repo/$path/$branch/OpenPetraSetup-$version.*.exe
+  do
+    if [ ! -f "$f" ]
+    then
+      break;
+    fi
+    release=`basename $f | awk -F. '{print $4}'`
+    if [ $release -ge $newrelease ]
+    then
+      newrelease=$((release+1))
+    fi
+  done
+fi
+version=$version"."$newrelease
+
 nant buildWindowsStandalone -D:OpenBuildService=true -D:ReleaseID=$version || exit -1
 
 mkdir -p ~/repo/$path/$branch
