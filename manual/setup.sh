6eec9eb38c099d26244d55fc16dc0bafc3092794
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2016-12-17 09:45:46 +0100

    build the openpetra manuals with LBS

---
+++ manual/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+dnf install -y make python3-sphinx git rsync
+# don't use graphviz yet
+
+if [ ! -d docs-en ]
+then
+  git clone --depth 1 https://github.com/openpetra/openpetra-docs.git docs-en
+  git clone --depth 1 https://github.com/openpetra/openpetra-docs-de.git docs-de
+fi
+
+cd ~/docs-en
+make html
+cd ~/docs-de
+make html
+cd ~
+
+#upload to Hostsharing
+if [ -f ~/.ssh/id_rsa_cronjob ]
+then
+  rsync -avz --delete -i ~/.ssh/id_rsa_cronjob docs-en/build/html tim00-openpetra@tim00.hostsharing.net:manual-en
+  rsync -avz --delete -i ~/.ssh/id_rsa_cronjob docs-de/build/html tim00-openpetra@tim00.hostsharing.net:manual-de
+fi
