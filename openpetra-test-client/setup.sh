e0d78eb9ca35c4f18e7d5e78baa6be779b64519f
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2019-11-23 21:39:04 +0100

    run nant test-client

--- openpetra-test-client/setup.sh
+++ openpetra-test-client/setup.sh
@@ -17,14 +17,13 @@ curl https://getopenpetra.com | bash -s devenv $ghubuser $branch $dbms || exit -
 cd /home/$user/openpetra
 
 # need this for the tests
-curl --silent --location https://github.com/openpetra/demo-databases/raw/$db_tag/demoWith1ledger.yml.gz > demoWith1ledger.yml.gz || exit -1
+demodbfile=demoWith1ledger.yml.gz
+curl --silent --location https://github.com/openpetra/demo-databases/raw/$db_tag/demoWith1ledger.yml.gz > $demodbfile || exit -1
 OP_CUSTOMER=$user /home/$user/openpetra-server.sh loadYmlGz $demodbfile || exit -1
 
 nant checkHtml || exit -1
 
-cd js-client
-# improve speed of initial request by user by forcing to load all assemblies now
-curl --silent --retry 5 http://localhost/api/serverSessionManager.asmx/IsUserLoggedIn # > /dev/null
-LANG=en CYPRESS_baseUrl=http://localhost ./node_modules/.bin/cypress run --config video=false || exit -1
+nant test-client || exit -1
+
 # we need a line feed so that the 0 is on the last line on its own for LBS to know that this succeeded
 echo
