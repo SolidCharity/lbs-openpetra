2004a09381eab9e249c50e22c80f52725d5c75e2
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2019-12-14 21:35:33 +0100

    build openpetra binary tarball

---
+++ openpetra-bin-tarball/setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+user=op_dev
+dbms=mysql
+branch=master
+if [ ! -z "$1" ]; then
+  branch=$1
+fi
+ghubuser=openpetra
+if [ ! -z "$2" ]; then
+  ghubuser=$2
+fi
+
+curl https://getopenpetra.com | bash -s devenv $ghubuser $branch $dbms || exit -1
+
+cd /home/$user/openpetra
+
+su $user -c "nant buildRelease" || exit -1
+
+for f in /home/$user/openpetra/delivery/openpetra-*-bin.tar.gz; do
+  tarball=`basename $f`
+  mv $f ~/tarball
+fi
+
+echo "DONE with building the tarball for " $branch
+echo "download at https://download.solidcharity.com/tarballs/solidcharity/openpetra-bin-tarball/$tarball"
+
+# we need a line feed so that the 0 is on the last line on its own for LBS to know that this succeeded
+echo
+
