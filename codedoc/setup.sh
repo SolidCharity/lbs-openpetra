ea34948c330b58b5a2a5597ff5f06c68c38d9176
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2021-11-09 16:43:20 +0100

    switch codedoc from Fedora to Debian

--- codedoc/setup.sh
+++ codedoc/setup.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-# get the key for the OpenPetra packages
-rpm --import "http://keyserver.ubuntu.com/pks/lookup?op=get&fingerprint=on&search=0x4796B710919684AC"
-dnf -y install mono-core mono-devel libgdiplus-devel nant wget tar sqlite doxygen openssh-clients || exit -1
+# get our own mono packages
+apt-get -y install apt-transport-https dirmngr gnupg ca-certificates
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0x4796B710919684AC
+echo 'deb [arch=amd64] https://download.solidcharity.com/repos/tpokorra/mono/debian/buster buster main' | tee /etc/apt/sources.list.d/mono-tpokorra.list
+echo 'deb [arch=amd64] https://download.solidcharity.com/repos/tpokorra/nant/debian/buster buster main' >> /etc/apt/sources.list
+echo 'deb [arch=amd64] https://download.solidcharity.com/repos/solidcharity/openpetra/debian/buster buster main' >> /etc/apt/sources.list
+apt-get update
+
+apt-get -y install nant mono-devel mono-xsp4 mono-fastcgi-server4 ca-certificates-mono xfonts-75dpi fonts-liberation libgdiplus doxygen openssh-client || exit -1
+
+# to avoid errors like: error CS0433: The imported type `System.CodeDom.Compiler.CompilerError' is defined multiple times
+if [ -f /usr/lib/mono/4.5-api/System.dll -a -f /usr/lib/mono/4.5/System.dll ]; then
+  rm -f /usr/lib/mono/4.5-api/System.dll
+fi
 
 wget https://github.com/openpetra/openpetra/archive/master.tar.gz
 tar xzf master.tar.gz
